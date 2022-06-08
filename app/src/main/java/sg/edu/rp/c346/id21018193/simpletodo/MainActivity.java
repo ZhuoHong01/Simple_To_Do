06ade702072e2b18d396ecadeae74583221a3771
lee zhuo hong <21018193@myrp.edu.sg>
2022-06-08 12:29:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/simpletodo/MainActivity.java
@@ -0,0 +1,54 @@
+package sg.edu.rp.c346.id21018193.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText tasks;
+    Button btnAdd;
+    Button btnClear;
+    ListView lvTasks;
+    ArrayList<String> alTasks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tasks = findViewById(R.id.editTextTask);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnClear = findViewById(R.id.buttonClear);
+        lvTasks = findViewById(R.id.listViewTasks);
+        alTasks = new ArrayList<>();
+
+        ArrayAdapter aaTask = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, alTasks);
+        lvTasks.setAdapter(aaTask);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String taskName = tasks.getText().toString();
+                alTasks.add(taskName);
+                aaTask.notifyDataSetChanged();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alTasks.clear();
+                aaTask.notifyDataSetChanged();
+            }
+        });
+    }
+}
